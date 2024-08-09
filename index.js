1ccb7ad6caf0637f374519688b2f7c101b15216b
yugamsehgal <yugamsehgal4254@gmail.com>
2024-08-09 12:33:23 +0530

    site update

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ window.addEventListener('load', function () {
   html.setAttribute("data-theme", theme);
   // set btn inner text
   var toggleThemeBtn = document.getElementById("toggle-theme-btn");
-  // toggleThemeBtn.innerText = theme === "light" ? "night mode: off" : "night mode: on";
 })
 
 function toggleTheme() {
@@ -21,24 +20,11 @@ function toggleTheme() {
   localStorage.setItem('theme', newTheme);
 
   var toggleThemeBtn = document.getElementById("toggle-theme-btn");
-  // toggleThemeBtn.innerText = theme === "light" ? "night mode: off" : "night mode: on";
 }
-
-// console.log(height);
-// var prevScrollpos = window.pageYOffset;
-// window.onscroll = function() {
-//   var currentScrollPos = window.pageYOffset;
-//   if (prevScrollpos > currentScrollPos) {
-//     document.getElementById("navbar").style.top = "0";
-//   } else {
-//     document.getElementById("navbar").style.top = "-50px";
-//   }
-//   prevScrollpos = currentScrollPos;
-// }
-
 
 var prevScrollpos = window.scrollY;
 window.onscroll = function () {
+  var theme = localStorage.getItem('theme');
   var navbar = document.getElementById("navbar")
   height = navbar.getBoundingClientRect().height;
   var currentScrollPos = window.scrollY;
@@ -46,6 +32,10 @@ window.onscroll = function () {
     navbar.style.top = "0";
   } else {
     navbar.style.top = "-" + height + "px";
+    navbar.style.boxShadow = theme === "dark" ? "0 1px 0 0 rgba(255,255,255,.2)" : "0 1px 0px 0 rgba(0,0,0,.2)";
   }
   prevScrollpos = currentScrollPos;
+  if (currentScrollPos <= 10) {
+    navbar.style.boxShadow = "0 0 0 0 rgba(0,0,0,0)";
+  }
 }
